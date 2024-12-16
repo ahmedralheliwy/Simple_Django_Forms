c8f87bd517f3e4b7ba029dd12f7d4c3a83c0a617
ahmedralheliwy <ahmed.alheliwy0.99@gmail.com>
2024-12-16 19:47:20 +0200

    customize sign up with bootstrap in forms.py ...commit 6

--- django_forms/sign_up/forms.py
+++ django_forms/sign_up/forms.py
@@ -5,3 +5,9 @@ class Sign_UpForm(forms.ModelForm):
     class Meta:
         model=Sign_Up
         fields='__all__'
+
+        widgets={
+           'username':forms.TextInput(attrs={'class':'form-control'}), 
+           'email':forms.EmailInput(attrs={'class':'form-control'}), 
+           'password':forms.PasswordInput(attrs={'class':'form-control'}), 
+        }
